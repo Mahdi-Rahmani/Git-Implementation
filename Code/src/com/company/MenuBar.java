638b98c0ac1fadb11b57ca8cb0c91fa21d757e29
Mahdi-Rahmani <mah.rah79@yahoo.com>
2022-03-11 23:21:36 +0330

    makeViewMenu method is added

--- Code/src/com/company/MenuBar.java
+++ Code/src/com/company/MenuBar.java
@@ -84,4 +84,43 @@ public class MenuBar extends JMenuBar {
         pssWordChange.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.ALT_MASK));
         userChange.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_MASK));
     }
+	/**
+     * The view menu operation
+     * you can toggle full screen or normal screen
+     */
+    private void makeViewMenu()
+    {
+        JMenuItem toggleFull = new JMenuItem("Toggle Full Screen");
+        toggleFull.setFont(new Font("Arial", Font.PLAIN, 12));
+        toggleFull.addActionListener(e -> {
+            if (toggleFull.getText().equals("Toggle Full Screen")) {
+                frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+                toggleFull.setText("Toggle Normal Screen");
+            }
+            else {
+                frame.setExtendedState(Frame.NORMAL);
+                toggleFull.setText("Toggle Full Screen");
+            }
+        });
+        toggleFull.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0));
+
+
+        JMenuItem toggleSidebar = new JMenuItem("Toggle Sidebar");
+        toggleSidebar.setFont(new Font("Arial", Font.PLAIN, 12));
+        toggleSidebar.addActionListener(e -> {
+            if (toggleSidebar.getText().equals("Toggle Sidebar")) {
+                toggleSidebar.setText("Toggle Normal");
+                frame.setLocation(0,0);
+                frame.setSize(700,1024);
+            }
+            else {
+                toggleSidebar.setText("Toggle Sidebar");
+                frame.setSize(1100, 600);
+                frame.setLocation(100, 200);
+            }
+        });
+        toggleSidebar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+        viewMenu.add(toggleSidebar);
+        viewMenu.add(toggleFull);
+    }
 }
